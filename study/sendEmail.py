a6a98f0d2c335c8ea5dddf04d041e9b87273d653
rinaissuper <596156210@qq.com>
2013-01-18 15:29:13 +0800

    python Email相关的一些方法

---
+++ study/sendEmail.py
@@ -0,0 +1,34 @@
+#-*-coding:utf
+#!/usr/bin/python
+#
+import smtplib
+from email.mime.text import MIMEText
+from urllib.request import urlopen
+
+#从文件读
+file_ob = open( '/home/mailcontent.txt' )
+try:
+    content = file_ob.read()
+finally:
+    file_ob.close()
+msg = MIMEText( content, 'plain', 'utf-8' )
+msg['Subject'] = 'this is a test from python'
+msg['From'] = 'from@163.com'
+msg['To'] = 'to@qq.com'
+
+#从网页读
+f = urlopen( 'http://www.pcesen.com' )
+msg = MIMEText( f.read(), 'html', 'utf-8' )
+msg['Subject'] = 'this is a test from python'
+msg['From'] = 'from@163.com'
+msg['To'] = 'to@qq.com'
+msg['Cc'] = mail_cc
+
+
+smtp = smtplib.SMTP()
+#协议
+smtp.connect( "smtp.163.com", "25" )
+#登陆
+smtp.login( 'from', 'password' )
+smtp.sendmail( 'from@163.com', 'to@qq.com', msg.as_string() )
+smtp.quit()
