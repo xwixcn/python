891ef359517f8b3790445e8bc5d2911d41f47cf6
rinaissuper <596156210@qq.com>
2013-03-11 14:03:42 +0800

    delete somefiles

---
+++ study/timeClass.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8
+#!/usr/bin/python
+import datetime
+import time
+import calendar
+def produce_timstamp():
+    '''
+    产生9位时间戳
+    '''
+    nowTime = datetime.datetime.now()
+    timestamp = int( time.mktime( nowTime.timetuple() ) )
+    return timestamp
+
+def lastmonth_Count():
+    '''
+    统计上个月的天数
+    '''
+    nowTime = datetime.datetime.now()
+    if nowTime.month == 1:
+        lastmonth = 12
+    else:
+        lastmonth = nowTime.month - 1
+    return calendar.monthrange( nowTime.year, lastmonth )[1]
+
