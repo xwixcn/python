7f7f32c8d11d82ba32e98dc72d625e5c9d9c7379
rinaissuper <596156210@qq.com>
2013-03-22 15:48:25 +0800

    nothing

--- study/emailMethod.py
+++ study/emailMethod.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.text import MIMEText
 from urllib.request import urlopen
 
-#从文件读
+
 file_ob = open( '/home/mailcontent.txt' )
 try:
     content = file_ob.read()
@@ -16,7 +16,7 @@ msg['Subject'] = 'this is a test from python'
 msg['From'] = 'from@163.com'
 msg['To'] = 'to@qq.com'
 
-#从网页读
+
 f = urlopen( 'http://www.pcesen.com' )
 msg = MIMEText( f.read(), 'html', 'utf-8' )
 msg['Subject'] = 'this is a test from python'
@@ -26,9 +26,9 @@ msg['Cc'] = mail_cc
 
 
 smtp = smtplib.SMTP()
-#协议
+
 smtp.connect( "smtp.163.com", "25" )
-#登陆
+
 smtp.login( 'from', 'password' )
 smtp.sendmail( 'from@163.com', 'to@qq.com', msg.as_string() )
 smtp.quit()
