418be0dbbc202ddaf2d3f4684fcff0d2d7d3ca1c
rinaissuper <596156210@qq.com>
2013-05-16 15:56:54 +0800

    无修改

--- work/mysqldataimport.py
+++ work/mysqldataimport.py
@@ -26,26 +26,27 @@ class DataImport:
 		if xlsfile.split(".")[-1] not in ["xls","csv"]:
 			print 'sorry,the fileformat is not correct'
 			sys.exit(1)
+		print xlsfile
 		data=self.parsexls(xlsfile)
-		self.conn()
-		cursor=self.conn.cursor()
-		i=0
-		for k,v in data.iteritems():
-			tagid=int(k)
-			for case,standcase in v.iteritems():
-				casemd5 = hashlib.md5(case).hexdigest()
-				try:
-					cursor.execute("insert into backend_case_info(name,md5,samecase) values(%s,%s,%s)",(case,casemd5,standcase) )
-					cursor.execute("select id from backend_case_info order by id desc limit 1")
-					caseid=int(cursor.fetchone()[0])
-					cursor.execute("insert into backend_case_taginfo(caseId_id,tagId_id) values(%s,%s)",(caseid,tagid))
-					print caseid
-					i+=1
-				except Exception, e:
-					print case+"failed!"
-					continue
-		print i
-		self.closeconn()
+ 		self.conn()
+ 		cursor=self.conn.cursor()
+ 		i=0
+ 		for k,v in data.iteritems():
+ 			tagid=int(k)
+ 			for case,standcase in v.iteritems():
+ 				casemd5 = hashlib.md5(case).hexdigest()
+ 				try:
+ 					cursor.execute("insert into backend_case_info(name,md5,samecase) values(%s,%s,%s)",(case,casemd5,standcase) )
+ 					cursor.execute("select id from backend_case_info order by id desc limit 1")
+ 					caseid=int(cursor.fetchone()[0])
+ 					cursor.execute("insert into backend_case_taginfo(caseId_id,tagId_id) values(%s,%s)",(caseid,tagid))
+ 					print caseid
+ 					i+=1
+ 				except Exception, e:
+ 					print case+"failed!"
+ 					continue
+ 		print i
+ 		self.closeconn()
 
 		
 
@@ -60,7 +61,7 @@ class DataImport:
 			sheet=excel.sheet_by_index(nsheet)
 			tagid=int(sheet.name)
 			casedic={}
-			#print sheet.nrows
+			print sheet.nrows
 			a=[]
 			for i in range(1,sheet.nrows):
 				case1=sheet.cell(i,0).value.encode("utf-8")
@@ -78,5 +79,5 @@ class DataImport:
 
 if __name__ == '__main__':
 	db=DataImport("172.20.23.75","root","123456","ontology")
-	db.importdata("case5.xls")
+	db.importdata("C:\\Users\\RINA\\Desktop\\Book1.xls")
 	#db.parsexls("case4.xls")
